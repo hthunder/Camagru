e8deb9a2335c3b46ae873292e9d4138acfd37b69
hthunder <Nosov_yura@inbox.ru>
2020-05-12 00:00:07 +0300

    first commit

---
+++ config/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	'news/([a-z]+)/([0-9]+)' => 'news/view/$1/$2',
+	// 'news/([0-9]+)' => 'news/view',
+	// 'news' => 'news/index',			// actionIndex в NewsController
+	// 'products' => 'product/list',	// actionList в ProductContoller
+	// 'news/archive' => 'news/archive',
+
+);
