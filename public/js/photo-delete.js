9f858bb333e9717a3c2e6087569149abb8264453
hthunder <Nosov.yura.web@gmail.com>
2020-07-22 15:00:32 +0300

    changed the confirmation text about deleting a photo

--- public/js/photo-delete.js
+++ public/js/photo-delete.js
@@ -2,7 +2,7 @@ let delete_form = document.querySelector(".page__delete-form");
 
 if (delete_form) {
     delete_form.onsubmit = function(){
-        if (confirm("Вы точно хотите удалить эту фотографию"))
+        if (confirm("Вы точно хотите удалить эту фотографию?"))
             this.submit();
         else
             event.preventDefault();
